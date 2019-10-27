8ad8bfda7d6fa4e3e6fe17f3afda1a069e532e3f
Andrew Zhao <steveand117@gmail.com>
2019-10-27 02:40:14 -0400

    Get the basic time graph working and formatted
    Uses mpAndroidChart to make pretty graphs :o

--- app/src/main/java/com/example/andrewzhao/myapplication/MainActivity.java
+++ app/src/main/java/com/example/andrewzhao/myapplication/MainActivity.java
@@ -6,6 +6,7 @@ import android.app.usage.UsageStatsManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.provider.Settings;
 import android.support.v4.app.NotificationCompat;
@@ -15,6 +16,7 @@ import android.util.Log;
 
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Collections;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
@@ -22,7 +24,18 @@ import java.util.Map;
 import android.R.drawable;
 
 import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.formatter.ValueFormatter;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+import com.github.mikephil.charting.utils.ColorTemplate;
+import com.github.mikephil.charting.utils.EntryXComparator;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -36,8 +49,8 @@ public class MainActivity extends AppCompatActivity {
                 android.os.Process.myUid(), getPackageName());
         if (!(mode == AppOpsManager.MODE_ALLOWED)) {
             requestForPerms = 1;
+            startActivityForResult(new Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS), requestForPerms);
         }
-        startActivityForResult(new Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS), requestForPerms);
         UsageStatsManager mUsageStatsManager = (UsageStatsManager) this.getSystemService(Context.USAGE_STATS_SERVICE);
         Calendar currentCalendar = Calendar.getInstance();
         Map<String, UsageStats> lUsageStatsMap = mUsageStatsManager.queryAndAggregateUsageStats(currentCalendar.getTimeInMillis()- 86400000, currentCalendar.getTimeInMillis());
@@ -57,18 +70,73 @@ public class MainActivity extends AppCompatActivity {
             }
         }
         LineChart chart = (LineChart) findViewById(R.id.chart);
-
+        Description desc = new Description();
+        desc.setText("");
+        chart.setDescription(desc);
+        chart.setGridBackgroundColor(rgb("#A277C4"));
+        chart.setDrawGridBackground(true);
+        chart.setDrawBorders(true);
+        // timesMap is the mock data
         // The key is the hours of sleep gotten, and the value is the day of the week
         Map<Float, Float> timesMap = new HashMap<>();
-        timesMap.put(7.5F, 1.0F);
-        timesMap.put(6.5F, 2.0F);
-        timesMap.put(8.5F, 3F);
+        timesMap.put(-3F, 1.0F);
+        timesMap.put(2F, 2.0F);
+        timesMap.put(-0.5F, 3F);
+        timesMap.put(-1F, 4F);
+        timesMap.put(0F, 5F);
+        timesMap.put(-0.2F, 6F);
+        timesMap.put(-0.3F, 0F);
         List<Entry> entries = new ArrayList<>();
+        // For an 8 hour recommendation
+        List<Entry> baselineSleep = new ArrayList<>();
+        baselineSleep.add(new Entry(0F,0F));
+        baselineSleep.add(new Entry(6F,0F));
         for (Map.Entry<Float, Float> singleTime : timesMap.entrySet()) {
-            entries.add(new Entry(singleTime.getKey(), singleTime.getValue()));
+            entries.add(new Entry(singleTime.getValue(), singleTime.getKey()));
         }
+        Collections.sort(entries, new EntryXComparator());
+        // the labels that should be drawn on the XAxis
+        final String[] weekdays = new String[] { "Mon", "Tue", "Wed", "Thur", "Fri", "Sat", "Sun" };
+        ValueFormatter formatter = new ValueFormatter() {
+            @Override
+            public String getAxisLabel(float value, AxisBase axis) {
+                return weekdays[(int) value];
+            }
+        };
+        XAxis xAxis = chart.getXAxis();
+        xAxis.setGranularity(1f); // minimum axis-step (interval) is 1
+        YAxis yAxis = chart.getAxisLeft();
+        xAxis.setValueFormatter(formatter);
+        LineDataSet dataSet = new LineDataSet(entries, null); // add entries to dataset
+        LineDataSet baseLineSleepDataSet = new LineDataSet(baselineSleep, null);
+        ArrayList<ILineDataSet> dataSets = new ArrayList<>();
+        dataSet.setColor(rgb("#7339A0"));
+        dataSet.setValueTextColor(rgb("#7339A0")); // styling, ...
+        dataSet.setCircleColor(rgb("#7339A0"));
+        dataSet.setLineWidth(2.5f);
+        dataSet.setCircleRadius(4f);
+        dataSet.setValueTextSize(12f);
+        baseLineSleepDataSet.setLineWidth(2.5f);
+        baseLineSleepDataSet.setCircleRadius(4f);
+        baseLineSleepDataSet.setColor(rgb("#FEFEF4"));
+        baseLineSleepDataSet.setValueTextColor(rgb("#FEFEF4"));
+        baseLineSleepDataSet.setCircleColor(rgb("#FEFEF4"));
+        dataSets.add(dataSet);
+        dataSets.add(baseLineSleepDataSet);
+        LineData lineData = new LineData(dataSets);
+        lineData.setDrawValues(false);
+
+        chart.setData(lineData);
+        chart.getLegend().setEnabled(false);
+        chart.invalidate(); // refresh
 
 
-
+    }
+    public static int rgb(String hex) {
+        int color = (int) Long.parseLong(hex.replace("#", ""), 16);
+        int r = (color >> 16) & 0xFF;
+        int g = (color >> 8) & 0xFF;
+        int b = (color >> 0) & 0xFF;
+        return Color.rgb(r, g, b);
     }
 }
