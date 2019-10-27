68d22ab42976a388eefde0ff67fe62e1d4f46926
Andrew Zhao <steveand117@gmail.com>
2019-10-26 21:46:59 -0400

    kinda added maps, but this is for you nathan
    gotchu

--- app/src/main/java/com/example/andrewzhao/myapplication/MainActivity.java
+++ app/src/main/java/com/example/andrewzhao/myapplication/MainActivity.java
@@ -13,12 +13,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
-import java.util.TimeZone;
 import android.R.drawable;
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -26,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.set_target);
         AppOpsManager appOps = (AppOpsManager) getSystemService(Context.APP_OPS_SERVICE);
         int mode = appOps.checkOpNoThrow(AppOpsManager.OPSTR_GET_USAGE_STATS,
                 android.os.Process.myUid(), getPackageName());
@@ -36,10 +40,9 @@ public class MainActivity extends AppCompatActivity {
         startActivityForResult(new Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS), requestForPerms);
         UsageStatsManager mUsageStatsManager = (UsageStatsManager) this.getSystemService(Context.USAGE_STATS_SERVICE);
         Calendar currentCalendar = Calendar.getInstance();
-
         Map<String, UsageStats> lUsageStatsMap = mUsageStatsManager.queryAndAggregateUsageStats(currentCalendar.getTimeInMillis()- 86400000, currentCalendar.getTimeInMillis());
         // DrawableUsageStatsMap is going to have the icon of the app with the usage states so that it can be easily displayed
-        Map<Drawable, UsageStats> drawableUsageStatsMap = new HashMap<Drawable, UsageStats>();
+        Map<Drawable, UsageStats> drawableUsageStatsMap = new HashMap<>();
         for (Map.Entry<String, UsageStats> entry : lUsageStatsMap.entrySet()) {
             Log.d("KEYS",("Entry keys: " + entry.getKey()));
             try {
@@ -53,7 +56,19 @@ public class MainActivity extends AppCompatActivity {
 
             }
         }
-        
-        setContentView(R.layout.landing);
+        LineChart chart = (LineChart) findViewById(R.id.chart);
+
+        // The key is the hours of sleep gotten, and the value is the day of the week
+        Map<Float, Float> timesMap = new HashMap<>();
+        timesMap.put(7.5F, 1.0F);
+        timesMap.put(6.5F, 2.0F);
+        timesMap.put(8.5F, 3F);
+        List<Entry> entries = new ArrayList<>();
+        for (Map.Entry<Float, Float> singleTime : timesMap.entrySet()) {
+            entries.add(new Entry(singleTime.getKey(), singleTime.getValue()));
+        }
+
+
+
     }
 }
---
+++ app/src/main/java/com/example/andrewzhao/myapplication/Map.java
@@ -0,0 +1,35 @@
+//package com.example.andrewzhao.myapplication;
+//
+//import com.github.mikephil.charting.charts.LineChart;
+//import com.github.mikephil.charting.data.Entry;
+//
+//import java.util.ArrayList;
+//import java.util.HashMap;
+//import java.util.List;
+//import android.os.Bundle;
+//import android.support.v4.app.Fragment;
+//import android.view.LayoutInflater;
+//import android.view.View;
+//import android.view.ViewGroup;
+//
+//public class Map extends Fragment {
+//    @Override
+//    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+//                             Bundle savedInstanceState) {
+//        // Inflate the layout for this fragment
+//        return inflater.inflate(R.layout.article_view, container, false);
+//    }
+//
+//    LineChart chart = (LineChart) findViewById(R.id.chart);
+//
+//    // The key is the hours of sleep gotten, and the value is the day of the week
+//    java.util.Map<Float, Float> timesMap = new HashMap<>();
+//        timesMap.put(7.5F, 1.0F);
+//        timesMap.put(6.5F, 2.0F);
+//        timesMap.put(8.5F, 3F);
+//    List<Entry> entries = new ArrayList<>();
+//    for(java.util.Map.Entry<Float, Float> singleTime : timesMap.entrySet()) {
+//        entries.add(new Entry(singleTime.getKey(), singleTime.getValue()));
+//    }
+//
+//}
